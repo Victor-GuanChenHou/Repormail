79aa1c9bccc2fdbc1520b6ff52386b6e2a843476
Victor-GuanChenHou <vfw0327@gmail.com>
2024-07-19 17:46:38 +0800

    modified:   FTPSgetdata.py

--- FTPSgetdata.py
+++ FTPSgetdata.py
@@ -1,18 +1,31 @@
 import paramiko
 from dotenv import load_dotenv
 import os
+import sys
+import logging
 load_dotenv()
+logging.basicConfig(
+    filename='sftp.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+
 # SFTP 連線資料
-hostname = os.getenv('Nashost')
-username = os.getenv('Nasusername')
-password = os.getenv('Naspassword')
-port = 22 
+try:
+    hostname = os.getenv('Nashost')
+    username = os.getenv('Nasusername')
+    password = os.getenv('Naspassword')
+    port = 22 
 
-# 創建SSH客戶端
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(hostname, port, username, password)
-
+    # 創建SSH客戶端
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname, port, username, password)
+    logging.info("Connect Success")
+except:
+    logging.info("Connect Error")
+    sys.exit()
 # 創建SFTP客戶端
 sftp = ssh.open_sftp()
 
