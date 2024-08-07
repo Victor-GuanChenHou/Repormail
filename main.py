54f08f80ec7a7ea186e427550bf49ab5149a8df8
Victor-GuanChenHou <vfw0327@gmail.com>
2024-08-07 16:28:16 +0800

    modified:   main.py

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 # coding: utf-8
 import os
-# os.system("python3 ./SFTPgetdata.py")
-# os.system("python3 ./Datamerge.py")
+import datetime
+os.system("python3 ./SFTPgetdata.py")
+os.system("python3 ./Datamerge.py")
 os.system('python3 ./ExcelCreater.py')
+wait=True
+while wait:
+    current_time = datetime.datetime.now()
+    am9 = current_time.replace(hour=9, minute=0, second=0, microsecond=0)
+    am930 = current_time.replace(hour=9, minute=30, second=0, microsecond=0)
+    if current_time>=am9 and current_time<=am930:
+        wait=False
 os.system('python3 ./reportmail.py')
